518d4cd20613e559b98e63cdffa7a4542bac7fd4
bobbylam <bobbyfreshh@gmail.com>
2015-03-12 20:27:45 -0700

    Implement random iPhone image on page load

--- js/6shot.js
+++ js/6shot.js
@@ -1,3 +1,11 @@
+// Randomly select iPhone comp image
+$(function() {
+  // var images = ['img/1.png', 'img/2.png', 'img/3.png', 'img/4.png', 'img/5.png'];
+  var images = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png'];
+  var randimg = images[Math.floor(Math.random() * images.length)];
+  $("#iPhone").attr("src", 'img/' + randimg);
+});
+
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 400) {
@@ -6,37 +14,6 @@ $(window).scroll(function() {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
-
-// //enable redacted text removal on touch
-// $(document).ready(function() {
-//     $('.hover').bind('touchstart', function(a) {
-//         a.preventDefault();
-//         $(this).toggleClass('hover_effect');
-//     });
-// });
-
-//jQuery to change image
-// $(window).scroll(function() {
-//     if ($(".navbar").offset().top > 70 && $(".navbar").offset().top <= 140) {
-//         $("#iPhone").attr("src", "img/2.png");
-//     }
-//     else if ($(".navbar").offset().top > 140 && $(".navbar").offset().top <= 210)  {
-//         $("#iPhone").attr("src", "img/3.png");
-//     }
-//     else if ($(".navbar").offset().top > 210 && $(".navbar").offset().top <= 280)  {
-//         $("#iPhone").attr("src", "img/4.png");
-//     }
-//     else if ($(".navbar").offset().top > 280)  {
-//         $("#iPhone").attr("src", "img/5.png");
-//     }
-//     else {
-//         $("#iPhone").attr("src", "img/1.png");
-//     }
-// });
-
-// $('#iPhone').on('click',function(){
-//     $("#iPhone").attr("src", "img/2.png");
-// });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
